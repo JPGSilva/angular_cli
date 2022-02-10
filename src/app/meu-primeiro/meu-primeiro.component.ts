3b3abe7b18bf71b338948fccf9aedb5852e27d56
joaopaulogsilva <jpsilva.math@gmail.com>
2022-02-10 07:18:37 -0400

    Tres Lagoas

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'meu-MeuPrimeiroComponent',
+    template: `
+    <p>Meu primeiro component com angular 2!</p>`
+})
+export class MeuPrimeiroComponent { }
